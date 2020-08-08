1b18b8635c4637c3402fbd50641e42563bb8976c
loganryan01 <61926209+loganryan01@users.noreply.github.com>
2020-08-08 16:38:14 +1000

    Updated AI diagrams, begun creating level map.

---
+++ Resources/myTilemap/Temple.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="Temple" tilewidth="16" tileheight="16" tilecount="3072" columns="64">
+ <image source="classical_temple_tiles.png" width="1024" height="768"/>
+</tileset>
